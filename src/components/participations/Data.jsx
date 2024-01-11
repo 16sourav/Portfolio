2265e935d7f65cf6c34fa3288df6983c236156c7
16sourav <126350102+16sourav@users.noreply.github.com>
2024-01-11 09:38:36 +0530

    Add files via upload

---
+++ src/components/participations/Data.jsx
@@ -0,0 +1,46 @@
+import Image1 from "../../assets/1682011589255.jpeg";
+import Image2 from "../../assets/1682697135602.jpeg";
+import Image3 from "../../assets/cert-coca.png.jpeg";
+import Image4 from "../../assets/cert-grid.png.jpeg";
+import Image5 from "../../assets/cert-tvs.png.jpeg";
+import Image6 from "../../assets/cert-grid2.png.jpeg"
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "",
+        description: "",
+    },
+    {
+        id: 2,
+        image: Image4,
+        title: "",
+        description: "",
+    },
+    {
+        id: 3,
+        image: Image5,
+        title: "",
+        description: "",
+    },
+    
+    {
+        id: 4,
+        image: Image6,
+        title: "",
+        description: "",
+    },
+    {
+        id: 5,
+        image: Image3,
+        title: "",
+        description: "",
+    },
+    {
+        id: 6,
+        image: Image2,
+        title: "",
+        description: "",
+    },
+];
